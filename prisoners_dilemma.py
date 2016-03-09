b12d9f5d950c1abfb5ec29ea73aa781949189d22
Maximum959 <1610>
2016-03-09 09:16:51 -0600

    Final algorithm
    Lasts algorithm from team 1

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -276,18 +276,16 @@ def get_action(player, history, opponent_history, score, opponent_score, getting
     #
     elif player == 7:
         if getting_team_name:
-            return 'loyal vengeful'
+            return 'rouge'
         else:
             # use history, opponent_history, score, opponent_score
             # to compute your strategy
-            if len(opponent_history)==0: #It's the first round: collude
-                return 'c'
+            if len(opponent_history)==0: #It's the first round: betrays
+                return 'b'
             elif history[-1]=='c' and opponent_history[-1]=='b':
                 return 'b' # betray is they were sucker last time
             else:
                 return 'c' #otherwise collude
-
-
 
 
 
